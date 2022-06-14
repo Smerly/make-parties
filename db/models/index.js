54dc73c75c772d7a997569d2154320f6b995b487
Ernest Cheung <ernestcheung02@gmail.com>
2022-06-14 16:59:44 -0700

    update for dani help

--- db/models/index.js
+++ db/models/index.js
@@ -8,7 +8,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
+// For Dani, this is where the problem is. How should I define Sequelize?
+let sequelize = new Sequelize();
 sequelize
 	.authenticate()
 	.then(() => {
